04c0bc8b79023d16ef8952b5f40947ab7ceb6948
Felipe <fdloopes@gmail.com>
2018-11-28 01:25:48 -0200

    primeiro commit

---
+++ Assets/Script/StreamingVideo.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExampleClass : MonoBehaviour
+{
+    public string filePath = System.IO.Path.Combine(Application.streamingAssetsPath, "https://www.youtube.com/watch?v=Ee1ZDX9dMbM");
+    public string result = "";
+    IEnumerator Example()
+    {
+        if (filePath.Contains("://"))
+        {
+            WWW www = new WWW(filePath);
+            yield return www;
+            result = www.text;
+        }
+        else
+            result = System.IO.File.ReadAllText(filePath);
+    }
+}
